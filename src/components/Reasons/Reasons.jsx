f7ee527ce78ef2e726328db51874a383bbff5190
Hamza TEYEB <teyeb.hamza12@gmail.com>
2022-09-02 17:55:37 +0200

    ad all block

--- src/components/Reasons/Reasons.jsx
+++ src/components/Reasons/Reasons.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import "./Reasons.css"
-import tick from "../../assets/tick.png"
-import image1 from "../../assets/image1.png"
-import image2 from "../../assets/image2.png"
-import image3 from "../../assets/image3.png"
-import image4 from "../../assets/image4.png"
+import tick from "../../assets/tick.png";
+import image1 from "../../assets/IMAGEE/fond2.png";
+import image2 from "../../assets/IMAGEE/fond1.jpeg";
+import image3 from "../../assets/IMAGEE/fond3.jpg";
+import image4 from "../../assets/IMAGEE/fond4.jpg";
+
 
 
 const Reasons = () => {
@@ -19,8 +20,30 @@ const Reasons = () => {
         </div>
 
         <div className="right-r">
+          <span>Some Reasons</span>
+          <div>
+          <span> Whay </span>
+          <span > Choose Us </span>
+          </div>
 
+          
+        <div className="details-r">
+          <div>
+            <img src={tick} alt="" />
+            <span>For more than 3 years we have been evolving in the web and digital marketing and offer our customers solutions that meet their needs and their projects without trying to sell them anything and everything. We prefer to build loyalty through the quality of our services and support than through purely contractual notions.</span>
+          </div>
+          <div>
+            <img src={tick} alt="" />
+            <span>The major asset of our digital marketing agency Moga Prod is undoubtedly the quality of our technical support, which combines both responsiveness and efficient responses.</span>
+          </div>
+          <div>
+            <img src={tick} alt="" />
+            <span>Working as a team for positive outcomes</span>
+          </div>
+         
         </div>
+        
+      </div>
     </div>
   )
 }
